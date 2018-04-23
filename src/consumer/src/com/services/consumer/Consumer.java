9b02f48e572fd2ee2280ef43894ba5f271ac3964
Kirill Shamanin <kirill.shamanin@profinit.eu>
2018-04-23 19:11:56 +0200

    Adapted consumer class for chapter 5.2.1. Listing 34.

--- src/consumer/src/com/services/consumer/Consumer.java
+++ src/consumer/src/com/services/consumer/Consumer.java
@@ -1,20 +1,18 @@
 package com.services.consumer;
 
-import com.services.provider.Provider;
-
 import java.lang.module.ModuleDescriptor;
-import java.util.ServiceLoader;
 import java.util.Set;
 
 public class Consumer {
 
     public static void main(String[] args) {
-        Iterable<Provider> providers = ServiceLoader.load(Provider.class);
+        Set<ModuleDescriptor.Requires> requires = Consumer.class.getModule().getDescriptor().requires();
+        for (ModuleDescriptor.Requires require : requires) {
+            System.out.println("Module name: " + require.name());
+            System.out.println("Modifiers: " + require.modifiers());
+            System.out.println("Compiled version: " + require.compiledVersion());
+            System.out.println("Raw compiled version: " + require.rawCompiledVersion());
 
-        for (Provider provider : providers) {
-            System.out.println(provider.getName());
         }
-
-
     }
 }
