eb9df076c5322802be18adbe39941056978978cf
Kirill Shamanin <kirill.shamanin@profinit.eu>
2018-04-23 19:14:21 +0200

    Adapted for Listing 36.

--- src/consumer/src/com/services/consumer/Consumer.java
+++ src/consumer/src/com/services/consumer/Consumer.java
@@ -1,18 +1,19 @@
 package com.services.consumer;
 
+import com.services.provider.Provider;
+
 import java.lang.module.ModuleDescriptor;
 import java.util.Set;
 
 public class Consumer {
 
     public static void main(String[] args) {
-        Set<ModuleDescriptor.Requires> requires = Consumer.class.getModule().getDescriptor().requires();
-        for (ModuleDescriptor.Requires require : requires) {
-            System.out.println("Module name: " + require.name());
-            System.out.println("Modifiers: " + require.modifiers());
-            System.out.println("Compiled version: " + require.compiledVersion());
-            System.out.println("Raw compiled version: " + require.rawCompiledVersion());
+        Set<ModuleDescriptor.Provides> provides = Provider.class.getModule().getDescriptor().provides();
 
+        for (ModuleDescriptor.Provides provide : provides) {
+            System.out.println("Service: " + provide.service());
+            System.out.println("Providers: " + provide.providers());
         }
+
     }
 }
